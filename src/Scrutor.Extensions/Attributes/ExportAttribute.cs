76406ac4b23a931248f7ac28fc1091d4bccce07f
Denis Pavlović <denis.pav@hotmail.com>
2019-10-07 20:32:32 +0200

    Added extension and attribute implementation

---
+++ src/Scrutor.Extensions/Attributes/ExportAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Scrutor.Extensions.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class ExportAttribute : Attribute
+    {
+        public readonly ServiceLifetime Type;
+
+        public ExportAttribute(ServiceLifetime type)
+        {
+            Type = type;
+        }
+    }
+}
---
+++ src/Scrutor.Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Scrutor.Extensions.Attributes;
+using System;
+using System.Reflection;
+
+namespace Scrutor.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterDependencies(this IServiceCollection services)
+        {
+            return services.Scan(
+                scan =>
+                {
+                    scan.FromApplicationDependencies()
+                        .AddClasses(classes => classes.Where(CreateFilter(ServiceLifetime.Scoped)))
+                        .AsImplementedInterfaces()
+                        .WithScopedLifetime()
+
+                        .AddClasses(classes => classes.Where(CreateFilter(ServiceLifetime.Transient)))
+                        .AsImplementedInterfaces()
+                        .WithTransientLifetime()
+
+                        .AddClasses(classes => classes.Where(CreateFilter(ServiceLifetime.Singleton)))
+                        .AsImplementedInterfaces()
+                        .WithSingletonLifetime();
+                }
+            );
+        }
+
+        internal static Func<Type, bool> CreateFilter(ServiceLifetime lifetime)
+        {
+            return type =>
+            {
+                var attributeType = type.GetCustomAttribute<ExportAttribute>()?.Type;
+
+                return attributeType.HasValue ? attributeType == lifetime : false;
+            };
+        }
+    }
+}
