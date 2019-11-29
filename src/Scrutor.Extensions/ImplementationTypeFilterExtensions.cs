ca827cf29e59a5302537081d6e16b1d5cdc88379
Denis Pavlović <denis.pav@hotmail.com>
2019-11-29 21:33:15 +0100

    Updated RegisterDependencies method

--- src/Scrutor.Extensions/ImplementationTypeFilterExtensions.cs
+++ src/Scrutor.Extensions/ImplementationTypeFilterExtensions.cs
@@ -7,6 +7,14 @@ namespace Scrutor.Extensions
 {
     internal static class ImplementationTypeFilterExtensions
     {
+        internal static IImplementationTypeSelector RegisterForLifetime(this IImplementationTypeSelector typeSelector, ServiceLifetime lifetime)
+        {
+            return typeSelector
+                .AddClasses(classes => classes.Filter(lifetime))
+                .AsImplementedInterfaces()
+                .WithLifetime(lifetime);
+        }
+
         internal static IImplementationTypeFilter Filter(this IImplementationTypeFilter classes, ServiceLifetime lifetime)
             => classes.Where(type => ValidateType(type, lifetime));
 
--- src/Scrutor.Extensions/ServiceCollectionExtensions.cs
+++ src/Scrutor.Extensions/ServiceCollectionExtensions.cs
@@ -7,21 +7,10 @@ namespace Scrutor.Extensions
         public static IServiceCollection RegisterDependencies(this IServiceCollection services)
         {
             return services.Scan(
-                scan =>
-                {
-                    scan.FromApplicationDependencies()
-                        .AddClasses(classes => classes.Filter(ServiceLifetime.Scoped))
-                        .AsImplementedInterfaces()
-                        .WithScopedLifetime()
-
-                        .AddClasses(classes => classes.Filter(ServiceLifetime.Transient))
-                        .AsImplementedInterfaces()
-                        .WithTransientLifetime()
-
-                        .AddClasses(classes => classes.Filter(ServiceLifetime.Singleton))
-                        .AsImplementedInterfaces()
-                        .WithSingletonLifetime();
-                }
+                scan => scan.FromApplicationDependencies()
+                    .RegisterForLifetime(ServiceLifetime.Scoped)
+                    .RegisterForLifetime(ServiceLifetime.Singleton)
+                    .RegisterForLifetime(ServiceLifetime.Transient)
             );
         }
     }
